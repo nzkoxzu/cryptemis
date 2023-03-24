488f4e895bd2b69967cccf9abe9ad3eee2c8b0d6
cr1ng3 <denis.remacle@pm.me>
2023-03-24 16:10:32 +0100

    added encryption and decryption functions

--- lib/decryption.dart
+++ lib/decryption.dart
@@ -1,1 +1,49 @@
-#placeholder
+import 'dart:convert';
+import 'package:kyber/kyber.dart';
+import 'package:pointycastle/pointycastle.dart';
+
+String decryptRSA(String ciphertext, String privateKey) {
+  final privateKeyBytes = base64.decode(privateKey);
+
+  final rsaPrivateKey = RSAKeyParser().parse(privateKeyBytes);
+
+  final ciphertextBytes = base64.decode(ciphertext);
+
+  final decryptor = RSAEngine()
+    ..init(false, PrivateKeyParameter<RSAPrivateKey>(rsaPrivateKey));
+  final plaintext = decryptor.process(ciphertextBytes);
+
+  final plaintextData = utf8.decode(plaintext);
+
+  return plaintextData;
+}
+
+String decryptAES(String ciphertext, String key) {
+  final keyBytes = base64.decode(key);
+
+  final aesKey = KeyParameter(keyBytes);
+  final aesCipher = BlockCipher('AES')..init(false, aesKey);
+
+  final ciphertextBytes = base64.decode(ciphertext);
+
+  final paddedPlaintext = aesCipher.process(ciphertextBytes);
+  final plaintextBytes = PKCS7Padding().strip(paddedPlaintext);
+
+  final plaintextData = utf8.decode(plaintextBytes);
+
+  return plaintextData;
+}
+
+String decryptKyber(String ciphertext, String privateKey) {
+  final privateKeyBytes = base64.decode(privateKey);
+
+  final kyber = Kyber(privateKey: privateKeyBytes);
+
+  final ciphertextBytes = base64.decode(ciphertext);
+
+  final plaintext = kyber.decrypt(ciphertextBytes);
+
+  final plaintextData = utf8.decode(plaintext);
+
+  return plaintextData;
+}
--- lib/encryption.dart
+++ lib/encryption.dart
@@ -1,1 +1,50 @@
-#placeholder
+import 'dart:convert';
+import 'package:kyber/kyber.dart';
+import 'package:pointycastle/pointycastle.dart';
+
+String encryptRSA(String plaintext, String publicKey) {
+  final publicKeyBytes = base64.decode(publicKey);
+
+  final rsaPublicKey = RSAPublicKeyParser().parse(publicKeyBytes);
+
+  final plaintextBytes = utf8.encode(plaintext);
+
+  final encryptor = RSAEngine()
+    ..init(true, PublicKeyParameter<RSAPublicKey>(rsaPublicKey));
+  final ciphertext = encryptor.process(plaintextBytes);
+
+  final ciphertextData = base64.encode(ciphertext);
+
+  return ciphertextData;
+}
+
+String encryptAES(String plaintext, String key) {
+  final keyBytes = base64.decode(key);
+
+  final aesKey = KeyParameter(keyBytes);
+  final aesCipher = BlockCipher('AES')..init(true, aesKey);
+
+  final plaintextBytes = utf8.encode(plaintext);
+
+  final paddedPlaintext =
+      PKCS7Padding().pad(plaintextBytes, aesCipher.blockSize);
+  final ciphertext = aesCipher.process(paddedPlaintext);
+
+  final ciphertextData = base64.encode(ciphertext);
+
+  return ciphertextData;
+}
+
+String encryptKyber(String plaintext, String publicKey) {
+  final publicKeyBytes = base64.decode(publicKey);
+
+  final kyber = Kyber(publicKey: publicKeyBytes);
+
+  final plaintextBytes = utf8.encode(plaintext);
+
+  final ciphertext = kyber.encrypt(plaintextBytes);
+
+  final ciphertextData = base64.encode(ciphertext);
+
+  return ciphertextData;
+}
